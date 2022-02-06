6ff01a1335fa9d0b3db878c0a7234f1db21cfa05
AMN69 <martinez.andreu@gmail.com>
2022-02-06 17:26:05 +0100

    20220206 testing typescript with The Net Ninja Course

--- sandbox.js
+++ sandbox.js
@@ -11,6 +11,13 @@ var mixed = ['ken', 4, 'chun-li', 8, 9];
 mixed.push('ryu');
 mixed.push(10);
 mixed[0] = 3;
+// AMN you can declare at the beginning an array of numbers and string and add later
+// a string ro number but you CAN'T add later a boolean. First you have to declare
+// a boolean as well.
+//mixed.push(true)
+//let mixed2 = ['ken', 4, true, 'chun-li', 8, false]
+//mixed2.push(true) // AMN I can do this but...
+//mixed2.push({ theKey: 'theValue'}) // AMN I can't do this.
 // objects
 var ninja = {
     name: 'mario',
@@ -21,6 +28,8 @@ ninja.age = 40;
 ninja.name = 'ryu';
 // ninja.age = '30';
 // ninja.skills = ['fighting', 'sneaking']
+// AMN - we CAN'T modify the keys of an array, we can redefine the object but not
+// modify the keys once created.
 ninja = {
     name: 'yoshi',
     belt: 'orange',
--- sandbox.ts
+++ sandbox.ts
@@ -17,6 +17,15 @@ mixed.push('ryu');
 mixed.push(10);
 mixed[0] = 3;
 
+// AMN you can declare at the beginning an array of numbers and string and add later
+// a string ro number but you CAN'T add later a boolean. First you have to declare
+// a boolean as well.
+//mixed.push(true)
+//let mixed2 = ['ken', 4, true, 'chun-li', 8, false]
+//mixed2.push(true) // AMN I can do this but...
+//mixed2.push({ theKey: 'theValue'}) // AMN I can't do this.
+
+
 // objects
 let ninja = {
   name: 'mario',
@@ -28,6 +37,9 @@ ninja.age = 40;
 ninja.name = 'ryu';
 // ninja.age = '30';
 // ninja.skills = ['fighting', 'sneaking']
+
+// AMN - we CAN'T modify the keys of an array, we can redefine the object but not
+// modify the keys once created.
 
 ninja = {
   name: 'yoshi',
