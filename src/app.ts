7f03a7274129ad4aa03187d3817b1af0eb4cc2de
AMN69 <martinez.andreu@gmail.com>
2022-03-06 16:13:32 +0100

    20220306 comments added for future understanding

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
+//AMN - Ts doesn't know (because it has no access) that there is the 
+// querySelector anchor tag. If we are sure that it is then we can avoid a mistake
+// by using the !. This ! says Ts that there is for sure an anchor tag.
 const anchor = document.querySelector('a')!;
 if(anchor) {
   console.log(anchor.href);
 }
 console.log(anchor.href);
 
+// AMN - Ts using ! defines the querySelector .new-item-form as an element
+// NOT as a HTMLFormElement because by the class name it doens't know the 
+// type of element .new-item-form is. But we can tell it to Ts by adding the
+// type, in this case we are grabbing an HTMLFormElement.
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
